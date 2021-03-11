ca788352970d69a6b7cfa57c2424b92af0cbdb2b
DeathbyToast <68164168+DeathbyToast@users.noreply.github.com>
2021-03-10 18:51:43 -0800

    Add files via upload

--- includes/footerDarkMode.js
+++ includes/footerDarkMode.js
@@ -1,4 +1,4 @@
-document.write('<footer><a href="https://discord.gg/UD4Temq">Questions? Suggestions?  Join Us on Discord!</a><br />\
+document.write('<footer><a href="https://discord.gg/UD4Temq">Questions? Suggestions?  Join Us on DISCOrd!</a><br />\
 <a href="https://www.patreon.com/join/PvPIVs" data-patreon-widget-type="become-patron-button"><img src="https://c5.patreon.com/external/logo/become_a_patron_button@2x.png" width=auto height="22"></a> &#124; PvP IVs ©2021</footer>\ ');
 
 /* Dark/Light Mode Theme Toggle Code*/
