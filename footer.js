259013431e5de272bada0752e6e7bd3f9414d313
DeathbyToast <68164168+DeathbyToast@users.noreply.github.com>
2021-10-27 17:32:07 -0600

    shortening text to not hide behind BMC widget

---
+++ footer.js
@@ -0,0 +1,29 @@
+document.write('<footer><a href="https://discord.gg/UD4Temq">Feedback: Join Us on Discord!</a> &#124; PvP IVs ©2021<br />\
+<script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="DeathbyToast" data-description="Support me on Buy me a coffee!" data-message="" data-color="#40DCA5" data-position="Right" data-x_margin="18" data-y_margin="18"></script></footer>\ ');
+
+/* Dark/Light Mode Theme Toggle Code*/
+document.getElementById('theme-toggle').addEventListener('change', () => {toggleTheme();});
+
+/* https://kevdees.com/adding-dark-mode-toggle-to-your-website/ */
+function toggleTheme() {
+  document.body.classList.toggle("dark-theme");
+  document.body.classList.toggle("light-theme");
+  document.getElementById("theme-toggle-icon").classList.toggle("flipped-icon");
+  
+  localStorage.setItem(
+    "colorTheme",
+    document.body.classList.contains("dark-theme") ? "dark-theme" : "light-theme"
+    );
+}
+
+/* Initialize the theme */
+if (localStorage.getItem("colorTheme") === "dark-theme") {
+  document.getElementById("theme-toggle").checked = true;
+  toggleTheme();
+} else if (
+    localStorage.getItem("colorTheme") !== "light-theme" &&
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+    toggleTheme();
+}
