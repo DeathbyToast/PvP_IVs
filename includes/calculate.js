77c56def2a96857586d7d9d94be39d7e2377ac7c
DeathbyToast <alex.gronbach@gmail.com>
2023-08-23 00:04:12 -0600

    Optimizing performance by skipping extra calcs

--- includes/calculate.js
+++ includes/calculate.js
@@ -2,8 +2,9 @@
 var cpm = [0.0939999967813491,0.135137430784308,0.166397869586944,0.192650914456886,0.215732470154762,0.236572655026622,0.255720049142837,0.273530381100769,0.29024988412857,0.306057381335773,0.321087598800659,0.335445032295077,0.349212676286697,0.36245774877879,0.375235587358474,0.387592411085168,0.399567276239395,0.41119354951725,0.422500014305114,0.432926413410414,0.443107545375824,0.453059953871985,0.46279838681221,0.472336077786704,0.481684952974319,0.490855810259008,0.499858438968658,0.508701756943992,0.517393946647644,0.525942508771329,0.534354329109191,0.542635762230353,0.550792694091796,0.558830599438087,0.566754519939422,0.574569148039264,0.582278907299041,0.589887911977272,0.59740000963211,0.604823657502073,0.61215728521347,0.61940411056605,0.626567125320434,0.633649181622743,0.640652954578399,0.647580963301656,0.654435634613037,0.661219263506722,0.667934000492096,0.674581899290818,0.681164920330047,0.687684905887771,0.694143652915954,0.700542893277978,0.706884205341339,0.713169102333341,0.719399094581604,0.725575616972598,0.731700003147125,0.734741011137376,0.737769484519958,0.740785574597326,0.743789434432983,0.746781208702482,0.749761044979095,0.752729105305821,0.75568550825119,0.758630366519684,0.761563837528228,0.764486065255226,0.767397165298461,0.77029727397159,0.77318650484085,0.776064945942412,0.778932750225067,0.781790064808426,0.784636974334716,0.787473583646825,0.790300011634826,0.792803950958807,0.795300006866455,0.79780392148697,0.800300002098083,0.802803892322847,0.805299997329711,0.807803863460723,0.81029999256134,0.812803834895026,0.815299987792968,0.817803806620319,0.820299983024597,0.822803778631297,0.825299978256225,0.827803750922782,0.830299973487854,0.832803753381377,0.835300028324127,0.837803755931569,0.840300023555755,0.842803729034748,0.845300018787384,0.847803702398935,0.850300014019012,0.852803676019539,0.85530000925064,0.857803649892077,0.860300004482269,0.862803624012168,0.865299999713897];
 
 function calculate(baseatk, basedef, basesta, floor, minLvl, maxLvl, invalid, league, mon) {
+  var T0, Tb, Ts, T1, Ts;
   if (perfTiming) {
-    var calculateT0 = performance.now();
+    T0 = performance.now();
     if (!(mon)) { mon = "";}
   }
   /* returns sorted list of all (up to 4096) combinations indexed by statProd + CP */
@@ -37,7 +38,10 @@ function calculate(baseatk, basedef, basesta, floor, minLvl, maxLvl, invalid, le
     		    continue; /* keep searching for level */
     		  }
     		  
-    		  var aSt = (baseatk + atk)*cpm[level];
+    		  /* Update maxLvl on first loop (0/0/0 or floor/floor/floor) to optimize performance */
+			  if (atk===floor/1 && def===floor/1 && sta===floor/1) {maxLvl = level;}
+			  
+			  var aSt = (baseatk + atk)*cpm[level];
     		  var dSt = (basedef + def)*cpm[level];
     		  var sSt = Math.floor((basesta + sta)*cpm[level]);
     		  var statProd = Math.round(aSt * dSt * sSt);
@@ -60,7 +64,7 @@ function calculate(baseatk, basedef, basesta, floor, minLvl, maxLvl, invalid, le
     		  
     		  var IVsum = atk/1 + def/1 + sta/1;
     		  var Star = "NA";
-          if (IVsum<23){Star="0*";}else if(IVsum<30){Star="1*";}else if (IVsum<37){Star="2*";}else if(IVsum<45){Star="3*";}else{Star="4*";}
+			  if (IVsum<23){Star="0*";}else if(IVsum<30){Star="1*";}else if (IVsum<37){Star="2*";}else if(IVsum<45){Star="3*";}else{Star="4*";}
     		  level = level/2 + 1;
     		  
     		  /* store as arrays to prevent hash collisions from dropping entires */
@@ -97,10 +101,10 @@ function calculate(baseatk, basedef, basesta, floor, minLvl, maxLvl, invalid, le
   }
   
   /* sort by statProd+CP before returning */
-  if (perfTiming) {var calculateTb = performance.now();/*console.log("calculate: Building took "+(calculateTb-calculateT0).toFixed(1)+"ms");*/}
+  if (perfTiming) {Tb = performance.now();}
   const sorted = {};
   Object.keys(ranks).sort(function(a, b){return b-a;}).forEach(function(key) {sorted[key] = ranks[key];});
-  if (perfTiming) {var calculateTs = performance.now();/*console.log("calculate: Sorting took "+(calculateTs-calculateTb).toFixed(1)+"ms");*/}
+  if (perfTiming) {Ts = performance.now();}
   
   /* add max stats to object */
   sorted.maxAtk = maxAtk;
@@ -115,7 +119,8 @@ function calculate(baseatk, basedef, basesta, floor, minLvl, maxLvl, invalid, le
   sorted.invalids = invalids;
   
   /* console.log("calculate output:"+JSON.stringify(sorted, null, 2)); */
-  if (perfTiming) {var calculateT1 = performance.now();
-  console.log(mon+" calculate: "+(calculateT1-calculateT0).toFixed(1)+"ms (Build "+(calculateTb-calculateT0).toFixed(1)+"ms + Sort "+(calculateTs-calculateTb).toFixed(1)+"ms) for "+numRanks+" ranks.");}
-  return sorted;
+  if (perfTiming) {T1 = performance.now();
+	console.log(mon+" calculate: "+(T1-T0).toFixed(1)+"ms (Build "+(Tb-T0).toFixed(1)+"ms + Sort "+(Ts-Tb).toFixed(1)+"ms) for "+numRanks+" ranks.");}
+	document.getElementById('timing_outputs').innerHTML += "calculate: "+(T1-T0).toFixed(1)+"ms (Build "+(Tb-T0).toFixed(1)+"ms + Sort "+(Ts-Tb).toFixed(1)+"ms) for "+numRanks+" ranks.<br>";
+	return sorted;
 }
